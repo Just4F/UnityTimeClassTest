8a2eafe95094f5335a82aaf89c02b124e6c5ab77
Ray^w^ <xray_mu@foxmail.com>
2018-06-15 17:46:17 +0800

    add

---
+++ UnityTimeTest/Assets/Script/TimeScaleTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class TimeScaleTest : MonoBehaviour {
+
+    public float _timeScaleSet;
+    public float TimeScaleSet
+    {
+        get
+        {
+            return _timeScaleSet;
+        }
+        set
+        {
+            _timeScaleSet = value;
+            Time.timeScale = _timeScaleSet;
+        }
+    }    
+
+    void AutoSetTimeScale()
+    {
+        if (TimeScaleSet != Time.timeScale)
+        {
+            Time.timeScale = TimeScaleSet;
+        }
+    }
+
+    static void Main()
+    {
+        var sdf = 22;
+        Debug.Log(sdf.GetNameValueFormart(_=>sdf));
+
+        //string abc = "123";
+        //int b = 1;
+        //test(() => abc);
+        //test(() => b);
+        //Console.ReadKey();
+    }
+
+
+	// Use this for initialization
+	void Start () {
+        Main();
+	}
+    
+
+	// Update is called once per frame
+	void Update () {
+        AutoSetTimeScale();
+        var sdf = 222;
+        //Utils.Nameof<int>(d => sdf);
+        Time.deltaTime.Nameof(_ => Time.deltaTime);
+        Utils.Nameof(_ => Time.deltaTime);
+
+    }
+
+    void FixedUpdate()
+    {
+        AutoSetTimeScale();
+    }
+
+
+
+    string Nameof<T>(System.Linq.Expressions.Expression<Func<T, T>> exp)
+    {
+        return Utils.Nameof(exp);
+    }
+
+    string GetNameValueFormart<T>(System.Linq.Expressions.Expression<Func<T, T>> exp,
+        string format = "{0} : {1}")
+    {
+        return Utils.GetNameValueFormart(exp, format);
+    }
+
+    string GetNameValueFormart1<T>(System.Linq.Expressions.Expression<Func<T, T>> exp)
+    {
+        return Utils.GetNameValueFormart1(exp);
+    }
+}
---
+++ UnityTimeTest/Assets/Script/Utils.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public static class Utils {
+
+    public static string Nameof<T>(System.Linq.Expressions.Expression<Func<T>> exp)
+    {
+        return ((System.Linq.Expressions.MemberExpression)exp.Body).Member.Name;
+    }
+
+    // 直接调用 T 的Type可能认不到需要显式指定<T>
+    public static string Nameof<T>(System.Linq.Expressions.Expression<Func<T, T>> exp)
+    {
+        return ((System.Linq.Expressions.MemberExpression)exp.Body).Member.Name;
+    }
+
+    /// <summary>
+    /// 扩展 获取变量名称(字符串)
+    /// lambda+反射实现, 比较慢  相当于C#6中的nameof
+    /// </summary>
+    /// <param name="var_name"></param>
+    /// <param name="exp"></param>
+    /// <returns>return string</returns>
+    public static string Nameof<T>(this T var_name, System.Linq.Expressions.Expression<Func<T, T>> exp)
+    {
+        return ((System.Linq.Expressions.MemberExpression)exp.Body).Member.Name;
+    }
+
+    public static string GetNameValueFormart<T>(System.Linq.Expressions.Expression<Func<T, T>> exp,
+    string format = "{0} : {1}")
+    {
+        var value = exp.Compile().Invoke(default(T));
+        return string.Format(format, Nameof(exp), value);
+    }
+
+    public static string GetNameValueFormart<T>(this T var_name, System.Linq.Expressions.Expression<Func<T, T>> exp,
+        string format = "{0} : {1}")
+    {
+        return string.Format(format, Nameof(exp), var_name.ToString());
+    }
+
+    public static string GetNameValueFormart1<T>(System.Linq.Expressions.Expression<Func<T, T>> exp)
+    {
+        return GetNameValueFormart(exp, "{0} : {1}, ");
+    }
+
+    public static string GetNameValueFormart1<T>(this T var_name, System.Linq.Expressions.Expression<Func<T, T>> exp)
+    {
+        return var_name.GetNameValueFormart(exp, "{0} : {1}, ");        
+    }
+}
